01dfce8357a1c653d8ec3f52fea12d21f4fb18d2
Gabriel da Silva Cardoso <cardoso.gabriel@proton.me>
2023-05-09 22:40:00 -0300

    Transforma gramática em LL1 e adiciona teste * Proibe IFs seguidos de IFs, forçando o uso de ELSE IFS * Verificar se STATEMENT_WITH_ELSE expressa corretamente a logica descrita (escrevi com sono)

---
+++ tests/test_ine5426.py
@@ -0,0 +1,6 @@
+from parsero import Parsero
+
+
+def test_ine5426():
+    parser = Parsero(f"examples/ConvCC-2023-1/regex.regex", f"examples/ConvCC-2023-1/gramatica.ghm", True)
+    parser.parse(f"examples/ConvCC-2023-1/exemplo1.lcc") # TODO
