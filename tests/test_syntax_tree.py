f139146336d3678012e20a51c95a1ced891d4c4c
Gabriel da Silva Cardoso <cardoso.gabriel@proton.me>
2023-06-11 12:47:45 -0300

    Code Lint

--- tests/test_syntax_tree.py
+++ tests/test_syntax_tree.py
@@ -1,5 +1,8 @@
 from parsero import Parsero
 
+
 def test():
-    parser = Parsero(f"examples/simple_syntax_tree/sst.regex", f"examples/simple_syntax_tree/sst.ghm12")
+    parser = Parsero(
+        f"examples/simple_syntax_tree/sst.regex", f"examples/simple_syntax_tree/sst.ghm12"
+    )
     parser.parse(f"examples/simple_syntax_tree/sst.example")
