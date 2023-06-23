4d6660ddad95173ec002b89d86a482682451c8e0
Nico Schmidt <nicoschmdt@proton.me>
2023-06-23 12:08:51 -0300

    Remove producões não condizentes da gramática EXPA e DEC, adiciona teste para EXPA

--- tests/test_expa.py
+++ tests/test_expa.py
@@ -5,10 +5,10 @@ def test():
     base = "examples/ConvCC-2023-1/"
     parser = Parsero(
         base + "regex.regex",
-        base + "gramatica_DEC.ghm",
+        base + "gramatica_EXPA.ghm",
         False,
         "examples/simple_syntax_tree/semantics/__init__.py",
     )
 
-    tree = parser.parse(base + "/DEC.example")
+    tree = parser.parse(base + "/EXPA.example")
     print(tree)
