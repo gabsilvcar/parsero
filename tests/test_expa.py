4e04553ac56b6bfccc08ff145c808dd5c9c6cc15
Nico Schmidt <nicoschmdt@proton.me>
2023-06-26 12:11:50 -0300

    Adiciona SDD de EXPA e DEC, atualiza teste de EXPA

--- tests/test_expa.py
+++ tests/test_expa.py
@@ -5,10 +5,11 @@ def test():
     base = "examples/ConvCC-2023-1/"
     parser = Parsero(
         base + "regex.regex",
-        base + "gramatica_EXPA.ghm",
+        base + "gramatica_EXPA.ghm12",
         False,
         "examples/simple_syntax_tree/semantics/__init__.py",
     )
 
     tree = parser.parse(base + "/EXPA.example")
+    parser.semantic_analysis(tree)
     print(tree)
